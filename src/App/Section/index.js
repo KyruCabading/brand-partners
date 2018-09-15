15632c38f111c77e479ac893460483ba5240c30a
Kyru Cabading <kyrusri@gmail.com>
2018-09-16 02:02:48 +0800

    CSS changes

--- src/App/Section/index.js
+++ src/App/Section/index.js
@@ -6,11 +6,11 @@ const style = {
     position: "relative",
     display: "block",
     textAlign: "left",
-    margin: '6vw 4vw 6vw 4vw',
+    margin: '4vw',
     fontSize: '70%',
     letterSpacing: 0.5,
     fontWeight: 100,
-    opacity: '0.8'
+    opacity: '0.9'
   }
 }
 
