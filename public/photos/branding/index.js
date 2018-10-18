b5cbca1e9d3065e483b2eb567a87a950d284b72e
Kyru Cabading <kyrusri@gmail.com>
2018-10-18 16:19:29 +0800

    Resize photos

--- public/photos/branding/index.js
+++
@@ -1,8 +0,0 @@
-import branding1 from './branding-1.jpg'
-import branding2 from './branding-2.jpg'
-import branding3 from './branding-3.jpg'
-import branding4 from './branding-4.jpg'
-
-const brandingImages = [branding1, branding2, branding3, branding4]
-
-export default brandingImages
