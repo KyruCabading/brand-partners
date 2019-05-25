5b19b87fd9ebbb04875efc7faf96beb890d75742
Kyru Cabading <kyrusri@gmail.com>
2019-05-25 21:10:33 +0800

    update margin spacing

--- src/App/Section/index.js
+++ src/App/Section/index.js
@@ -1,28 +1,30 @@
-import React from 'react'
-import { Typography } from '@material-ui/core'
+import React from "react";
+import { Typography } from "@material-ui/core";
 
 const style = {
   heading: {
-    fontSize: '15px'
+    fontSize: "15px"
   },
   section: {
     position: "relative",
     display: "block",
     textAlign: "left",
-    margin: '6vw 4vw',
-    fontSize: '70%',
+    margin: "4vh 4vw",
+    fontSize: "70%",
     letterSpacing: 0.5,
     fontWeight: 100,
-    opacity: '0.9'
+    opacity: "0.9"
   }
-}
+};
 
 export default props => {
   return (
     <div style={style.section}>
-      <Typography style={style.heading} variant="title">{props.title}</Typography>
+      <Typography style={style.heading} variant="title">
+        {props.title}
+      </Typography>
       <Typography>{props.content}</Typography>
       {props.children}
     </div>
-  )
-}
+  );
+};
