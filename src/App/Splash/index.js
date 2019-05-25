d59497c18343da4800b3a44d0613b009f09856f3
Kyru Cabading <kyrusri@gmail.com>
2019-05-25 19:37:51 +0800

    add smoke loading

--- src/App/Splash/index.js
+++ src/App/Splash/index.js
@@ -19,71 +19,7 @@ const Splash = props => {
   let params = new URLSearchParams(location.search);
   return (
     <div className={classes.container}>
-      <Particles
-        height={300}
-        params={{
-          fps_limit: 28,
-          particles: {
-            number: {
-              value: 200,
-              density: {
-                enable: false
-              }
-            },
-            line_linked: {
-              enable: true,
-              distance: 30,
-              opacity: 0.4
-            },
-            move: {
-              speed: 1
-            },
-            opacity: {
-              anim: {
-                enable: true,
-                opacity_min: 0.05,
-                speed: 2,
-                sync: false
-              },
-              value: 0.4
-            }
-          },
-          polygon: {
-            enable: true,
-            scale: 0.5,
-            type: "inline",
-            move: {
-              radius: 10
-            },
-            url: "https://image.flaticon.com/icons/svg/32/32425.svg",
-            inline: {
-              arrangement: "equidistant"
-            },
-            draw: {
-              enable: true,
-              stroke: {
-                color: "rgba(255, 255, 255, .2)"
-              }
-            }
-          },
-          retina_detect: false,
-          interactivity: {
-            events: {
-              onhover: {
-                enable: true,
-                mode: "bubble"
-              }
-            },
-            modes: {
-              bubble: {
-                size: 6,
-                distance: 40
-              }
-            }
-          }
-        }}
-        className="animated fadeIn"
-      />
+      <img src="https://cdn.dribbble.com/users/1106178/screenshots/4175222/orb.gif" />
       <Typography variant="caption" className="animated fadeInStagger">
         Manus X Winston
       </Typography>
